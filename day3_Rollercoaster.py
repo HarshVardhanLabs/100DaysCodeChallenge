ed9b34abedd2eb6823c92eb8f1c7f69b1f29b382
Harsh Vardhan <harshvardhan02102002@gmail.com>
2024-11-20 18:30:08 +0530

    Day3 Roller coaster

---
+++ day3_Rollercoaster.py
@@ -0,0 +1,6 @@
+print("Welcome to the Rollercoaster!")
+height = int(input("What is Your Height in cm? "))
+if height>=120:
+    print("You are Eligible to ride ! Happy Riding and Enjoy")
+else:
+    print("Grow Taller first sorry !")    
