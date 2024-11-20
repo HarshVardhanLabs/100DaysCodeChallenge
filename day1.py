28e8dad5c48b76b3e243f61f9efa37a41186739d
Harsh Vardhan <harshvardhan02102002@gmail.com>
2024-11-20 06:11:53 +0000

    day 1 code is pushed

--- day1.py
+++ day1.py
@@ -1,1 +1,5 @@
-print("hello world")
+print("Welcome to the Band namane Genrator.")
+city = input("What is your birth Place in city? \n")
+pet= input("What is your Pet Name? \n")
+band_name = city+" "+pet
+print("Than your band Name Could be "+band_name)
